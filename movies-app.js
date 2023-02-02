fb6cd08f57c32ff1c98e3f5716d5c45fad16f0e6
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-02-02 16:26:00 -0600

    notes for tonight

--- movies-app.js
+++ movies-app.js
@@ -83,3 +83,4 @@ function deleteMovie() {
 $('#movie-table-1').on("click", "button.delete-btn", function () {
     var buttonID = $(this).attr("data-id");
 });
+// trying to mess around with the delete movie function
